10c1856dee8b3356c4c83e4886e8e1743ccde9b4
Mariya Lubashevskaya <selestina84@gmail.com>
2020-03-21 19:21:15 +0500

    lesson2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let money = prompt("Ваш бюджет на месяц?", '');
+let money = +prompt("Ваш бюджет на месяц?", '');
 let time = prompt("Введите дату в формате YYYY-MM-DD", '');
 let appData = {
     budget: money,
@@ -10,12 +10,23 @@ let appData = {
     income: [],
     savings: false    
 };
-let b1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    b2 = prompt("Во сколько обойдется?", ''),
-    b3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    b4 = prompt("Во сколько обойдется?", '');
+    for (let i = 0; i < 2; i++){
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = +prompt("Во сколько обойдется?", '');
+        if((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && b != '' && a.length < 50){
+            appData.expenses[a] = b; 
+        } else {
+            alert ('Вводите данные правильно');
+        };
+       
+    };
+    appData.moneyPerDay = appData.budget/30
 
-appData.expenses[b1] = b2;
-appData.expenses[b3] = b4;
-
-alert(appData.budget/30);
+alert(`Ежедневный бюджет  ${appData.moneyPerDay}`);
+if (appData.moneyPerDay < 100) {
+    alert('Минимальный уровень достатка');
+} else if(appData.moneyPerDay >= 100 && appData.moneyPerDay < 1000){
+    alert('Средний уровень достатка');
+} else if(appData.moneyPerDay > 1000){
+    alert('Высокий уровень уровень достатка');
+} else { alert('Ошибка, переданы неверные данные')}
